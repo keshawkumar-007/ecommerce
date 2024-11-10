f227b6769b13a38b3e78b2e4682a03c854433d2a
sunilip <sunil.digwal@ichhapurti.com>
2024-11-10 10:39:46 +0530

    new code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
 import './App.css';
+import About from './Pages/About/About';
+import Productlisting from './Pages/Product_listing/Productlisting';
+import Productdetail from './Pages/Product_detail/Productdetail';
+import Cart from './Pages/Cart/Cart';
+import Checkout from './Pages/Checkout/Checkout';
+import Contact from './Pages/Contact/Contact';
+import Loginorregister from './Pages/Login_register/Loginorregister';
+import Blog from './Pages/Blog_list/Blog';
+import Blogsingle from './Pages/Blog-single/Blogsingle';
+import Error from './Pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/product_listing" element={<Productlisting />} />
+    <Route path="/product_detail" element={<Productdetail />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/checkout" element={<Checkout />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/login_register" element={<Loginorregister />} />
+    <Route path="/blog" element={<Blog />} />
+    <Route path="/blog-single" element={<Blogsingle />} />
+    <Route path="/error" element={<Error />} />
+  </Routes>
+  </>
   );
 }
 
